851f323cd710a32e03dc61dbb2846e305aadc66e
Adrian Huang <sorcereradrian@gmail.com>
2025-12-23 19:40:37 +0800

    add prettier

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,11 +1,11 @@
-import SwaggerUI from "swagger-ui-react";
+import SwaggerUI from 'swagger-ui-react'
 
-import logoLight from "./logo-light.svg";
+import logoLight from './logo-light.svg'
 
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+      <div className="flex min-h-0 flex-1 flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
           <div className="w-125 max-w-[100vw] p-4">
             <img
@@ -18,5 +18,5 @@ export function Welcome() {
         <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
       </div>
     </main>
-  );
+  )
 }
